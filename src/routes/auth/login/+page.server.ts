b89bbacc1ec477e8696d3d0a1496cf6c50239f3e
PILTRAFILLA317 <unaimartinm@gmail.com>
2025-01-15 16:10:38 +0100

    main page

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.user) {
-		return redirect(302, '/demo/lucia');
+		return redirect(302, '/');
 	}
 	return {};
 };
@@ -51,7 +51,7 @@ export const actions: Actions = {
 		const session = await auth.createSession(sessionToken, existingUser.id);
 		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
 
-		return redirect(302, '/demo/lucia');
+		return redirect(302, '/');
 	},
 	register: async (event) => {
 		const formData = await event.request.formData();
@@ -103,7 +103,7 @@ export const actions: Actions = {
 			console.error('Error inserting user:', error);
 		}
 
-		return redirect(302, '/demo/lucia');
+		return redirect(302, '/');
 	}
 };
 
