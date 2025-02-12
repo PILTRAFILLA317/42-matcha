371cf341bea0e79c1294a8a4be25530bfc687667
PILTRAFILLA317 <unaimartinm@gmail.com>
2025-02-12 18:55:47 +0100

    pre merge

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,8 +4,4 @@ if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export const db = postgres(env.DATABASE_URL, {
     ssl: { rejectUnauthorized: false },
-    // ssl: 'require',
-    // max: 200,
-    // connect_timeout: 15,
-    // idle_timeout: 30
 });
--- src/routes/api/notifications/stream/+server.ts
+++ src/routes/api/notifications/stream/+server.ts
@@ -1,7 +1,6 @@
 // src/routes/api/sse/+server.ts
 import { db } from '$lib/server/db';
 import type { RequestHandler } from './$types';
-import { dev } from '$app/environment';
 
 // Track active listeners per user
 const activeListeners = new Map<
@@ -21,7 +20,6 @@ async function setupGlobalListener() {
   console.log('Global listener not set up, setting up now');
 
   globalListener = db.listen(`user_notifications`, (payload) => {
-    // const userId = channel.replace('user_notifications_', '');
     const userId = JSON.parse(payload).userID;
     const listener = activeListeners.get(userId);
 
